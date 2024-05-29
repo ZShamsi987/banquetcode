4ec46b3204c1b3a24e86be088a0744aec82ccb18
Zafir Shamsi <79466845+ZShamsi987@users.noreply.github.com>
2024-05-29 18:13:45 -0400

    Create systems.hpp

---
+++ include/systems.hpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+extern pros::Controller master;
+
+extern Motor left_mtr1;
+extern Motor left_mtr2;
+extern Motor left_mtr3;
+extern Motor right_mtr1;
+extern Motor right_mtr2;
+extern Motor right_mtr3;
+
+extern Motor intake_mtr;
+
+extern Motor cata_mtr;
+
+extern pros::ADIDigitalOut wings;  
